82a38246790d8f7311562fa819f523f8b1b9b346
fanc <fanaticheng@foxmail.com>
2016-10-15 18:00:36 +0800

    把c++的界面改一下
    因为必须要有对象才能够显示对象里面的成员

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@ int main(){
         scanf("%d",&option);
         switch(option){
             case 1:{
-                int choose1;
-                int on1=1;
-                while(on1){
+                int choose;
+                int on=1;
+                while(on){
                     system("clear");
                     printf("\n\n");
                     printf("      Menu for POSTK STACK STRUCTURE       \n");
@@ -40,8 +40,8 @@ int main(){
                     printf("    	  0. Exit\n");
                     printf("-------------------------------------------------\n");
                     printf("    请选择你的操作[0~10]:\n");
-                    scanf("%d",&choose1);
-                    switch(choose1){
+                    scanf("%d",&choose);
+                    switch(choose){
                         case 1:{
                             int m;
                             printf("请输入栈的最大元素个数:\n");
@@ -126,7 +126,7 @@ int main(){
                             break;
                         }
                         case 0:{
-                            on1=0;
+                            on=0;
                             break;
                         }
                     }//end of switch
@@ -138,8 +138,126 @@ int main(){
                 start=0;
                 break;
             }
-        }//end of swith
+            case 2:{
+                OOSTK TK1(0);   //防止没有对象
+                int choose;
+                int on=1;
+                while(on){
+                    system("clear");
+                    printf("\n\n");
+                    printf("      Menu for POSTK STACK STRUCTURE       \n");
+                    printf("-------------------------------------------------\n");
+                    printf("    	  1. OOSTK           2. size     \n");
+                    printf("    	  3. howMany         4. getelem  \n");
+                    printf("    	  5. push            6. pop      \n");
+                    printf("    	  7. OOSTKbyS        8. print    \n");
+                    printf("    	  9. assign          10.~OOSTK   \n");
+                    printf("    	  0. Exit\n");
+                    printf("-------------------------------------------------\n");
+                    printf("    请选择你的操作[0~10]:\n");
+                    scanf("%d",&choose);
+                    switch(choose){
+                        case 1:{
+                            int m;
+                            std::cout<<"请输入栈的最大元素个数"<<std::endl;
+                            std::cin>>m;
+                            OOSTK TK1(m);
+                            std::cout<<"栈创建成功"<<std::endl;
+                            std::cout<<"当前栈的最大容量为"<<TK1.size()<<std::endl;
 
+                            getchar();getchar();
+                            break;
+                        }
+                        case 2:{
+                            std::cout<<"当前栈的最大容量为"<<TK1.size()<<std::endl;
+                            getchar();getchar();
+                            break;
+                        }
+                        case 3:{
+                            printf("当前栈的元素个数为:%d\n",howMany(A));
+                            getchar();getchar();
+                            break;
+                        }
+                        case 4:{
+                            int x;
+                            printf("输入想要访问的下标x:");
+                            scanf("%d",&x);
+                            printf("下标%d对应的元素为:%d\n",x,getelem(A,x));
+                            getchar();getchar();
+                            break;
+                        }
+                        case 5:{
+                            int e=0;
+                            printf("请输入即将入栈的数:");
+                            scanf("%d",&e);
+                            if(A->pos==A->max)
+                                printf("插入失败，栈元素已满\n");
+                            else{
+                                A=push(A, e);
+                                printf("入栈成功!\n");
+                            }
+                            getchar();getchar();
+                            break;
+                        }
+                        case 6:{
+                            int elem;
+                            A=pop(A,elem);
+                            printf("出栈的数据为:%d\n",elem);
+                            printf("出栈成功!\n");
+                            getchar();getchar();
+                            break;
+                        }
+                        case 7:{
+                            
+                            if(!initPOSTKS(&S))
+                                printf("拷贝失败\n");
+                            else{
+                                initPOSTK(A, S);
+                                printf("栈初始化成功，成功分配储存空间\n");
+                            }
+                            getchar();getchar();
+                            break;
+                        }
+                        case 8:{
+                            print(A);
+                            getchar();getchar();
+                            break;
+                        }
+                        case 9:{
+                            if(!initPOSTKS(&S))
+                                printf("拷贝失败\n");
+                            else{
+                                A=assign(A, S);
+                                printf("拷贝成功，将S的元素成功拷贝\n");
+                            }
+                            getchar();getchar();
+                            break;
+                        }
+                        case 10:{
+                            destroyPOSTK(A);
+                            printf("摧毁栈成功\n");
+                            getchar();getchar();
+                            break;
+                        }
+                        case 0:{
+                            on=0;
+                            break;
+                        }
+                    }//end of switch
+                }//end of while
+                printf("欢迎下次测试功能1\n");
+                break;
+            }//end of case 2
+            case 3:{
+                break;
+            }
+            case 4:{
+                break;
+            }
+            case 5:{
+                break;
+            }
+        }//end of swith
     }//end of while
     printf("欢迎下次测试本系统\n");
     return 0;
